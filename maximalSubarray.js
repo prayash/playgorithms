a6d0e3d85abba59f6cee85b5b7124642c70e56e6
Prayash Thapa <prayasht@gmail.com>
2017-01-18 14:01:57 -0700

    finish stock prices problem

--- maximalSubarray.js
+++ maximalSubarray.js
@@ -1,10 +1,10 @@
 'use strict';
 
 /**
- * Maximal Subarray
+ * Maximal Subarray -  O(n^3) time / O(1) subproblemace
  * Given an array of integers (positive/zero/negative), write a function that
  * calculates the maximum sum of any continuous subarray of the input array.
- * @param {Array} A - array.
+ * @param {Array} A - the input array
  * @return {int} - max sum of a continuous subarray
  */
 function maximalSubarrayNaive(A) {
@@ -27,10 +27,35 @@ function maximalSubarrayNaive(A) {
   return max;
 }
 
+/**
+ * Maximal Subarray -  O(n) time / O(n) space
+ * Original Problem - Find the maximum sum of any subarray in the range [0...n-1]
+ * Subproblem - Find the maximum sum of any subarray starting at any index in the range [0...i] and ending at i
+ * Recurrence is subproblem[i] = max(subproblem[i - 1] + array[i], 0)
+ * @param {Array} A - the input array
+ * @return {int} - max sum of a continuous subarray
+ */
 function maximalSubarrayDP(A) {
-  return;
+  var max = 0;
+  var subproblem = [];
+
+  subproblem[0] = Math.max(A[0], 0);
+  for (var i = 1; i < A.length; i++) {
+    subproblem[i] = Math.max(subproblem[i - 1] + A[i], 0);
+  }
+
+  // Linear search along table to grab final answer
+  for (var j = 0; j < A.length; j++) {
+    max = Math.max(max, subproblem[j]);
+  }
+
+  console.log(max);
+  return max;
 }
 
 var A = [-1, 5, 100, -1000];
 maximalSubarrayNaive(A);
 maximalSubarrayDP(A);
+
+
+module.exports = maximalSubarrayDP;
